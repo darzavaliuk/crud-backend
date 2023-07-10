309a3137b10113828e1db3d29b5cb80054568e1e
Daria <dar.zavaliuk@gmail.com>
2023-07-10 17:59:30 +0300

    feat: add snippet for server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("start")
+const express = require('express')
+
+const app = express();
+
+app.get("/", (req, res) => {
+    console.log("start")
+})
+
+const port = 3005;
+
+app.listen(port, () => console.log("Server is running on port: " + port))
