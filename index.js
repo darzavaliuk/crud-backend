07ba89f7d0b6cfbbacdbc95599399e411212341f
Daria <dar.zavaliuk@gmail.com>
2023-07-10 18:40:24 +0300

    feat: add connection to database

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
-
+const dotenv = require('dotenv')
 const app = express();
+const jwt = require("jsonwebtoken")
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017")
+    .then(() => console.log("connect to db"))
+    .catch(er => console.log(er))
+
+app.use(express.json())
 app.get("/", (req, res) => {
     console.log("start")
+    res.send("Hello World")
+})
+app.post("/auth/login", (req, res) => {
+    console.log(req.body)
+    const token = jwt.sign(req.body, "12345678")
+    res.json({"success": "yes", "token": token})
 })
 
-const port = 3005;
-
+dotenv.config()
+const port = process.env.PORT || 3006;
 app.listen(port, () => console.log("Server is running on port: " + port))
